e89c1627e04c11b63dd634f1862f9f383a433fcb
lijing <lijing.168@dcarlife.com>
2024-08-07 11:11:51 +0800

    feat: 新增 ts-node nodemon

--- packages/formily-study/src/how-to-use/index.ts
+++ packages/formily-study/src/how-to-use/index.ts
@@ -1,5 +1,9 @@
-import { observable } from "@formily/reactive";
+import { autorun, observable } from "@formily/reactive/src/index";
 
 const observableData = observable({
-    
-})
+  age: 18,
+});
+
+autorun(() => {
+  console.log(observableData.age);
+});
